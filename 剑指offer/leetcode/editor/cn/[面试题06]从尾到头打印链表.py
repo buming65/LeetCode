da4eb3b1309c5c255a9815a7d1c3329e81813d7d
buming65 <35394066+buming65@users.noreply.github.com>
2020-05-25 16:09:20 -0700

    offer

---
+++ 剑指offer/leetcode/editor/cn/[面试题06]从尾到头打印链表.py
@@ -0,0 +1,27 @@
+# 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入：head = [1,3,2]
+# 输出：[2,3,1] 
+# 
+#  
+# 
+#  限制： 
+# 
+#  0 <= 链表长度 <= 10000 
+# 
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+# leetcode submit region end(Prohibit modification and deletion)
---
+++ 剑指offer/leetcode/editor/cn/[面试题20]表示数值的字符串.py
@@ -0,0 +1,13 @@
+# 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"0123"都表示数值，但
+# "12e"、"1a3.14"、"1.2.3"、"+-5"、"-1E-16"及"12e+5.4"都不是。 
+# 
+#  
+# 
+#  注意：本题与主站 65 题相同：https://leetcode-cn.com/problems/valid-number/ 
+#  Related Topics 数学
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isNumber(self, s: str) -> bool:
+# leetcode submit region end(Prohibit modification and deletion)
